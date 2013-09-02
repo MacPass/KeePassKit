542f282d8d6a8c8a8776be0714992f3a0723efb8
michael starke <michael.starke@hicknhack-software.com>
2013-09-03 01:25:01 +0200

    Fixed default auto type and search settings for groups

--- Core/KPKGroup.m
+++ Core/KPKGroup.m
@@ -44,6 +44,8 @@
   if (self) {
     _groups = [[NSMutableArray alloc] initWithCapacity:8];
     _entries = [[NSMutableArray alloc] initWithCapacity:16];
+    _isAutoTypeEnabled = KPKInherit;
+    _isSearchEnabled = KPKInherit;
     self.updateTiming = YES;
   }
   return self;
