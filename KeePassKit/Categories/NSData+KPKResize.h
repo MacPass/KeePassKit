4715ce5d2584bb2e5757dc09aaafa7b5768bda7c
michael starke <michael.starke@hicknhack-software.com>
2016-11-01 22:11:46 +0100

    updated KissXML

---
+++ KeePassKit/Categories/NSData+KPKResize.h
@@ -0,0 +1,13 @@
+//
+//  NSData+KPKResize.h
+//  KeePassKit
+//
+//  Created by Michael Starke on 01/11/2016.
+//  Copyright © 2016 HicknHack Software GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSData (KPKResize)
+
+@end
---
+++ KeePassKit/Categories/NSData+KPKResize.m
@@ -0,0 +1,13 @@
+//
+//  NSData+KPKResize.m
+//  KeePassKit
+//
+//  Created by Michael Starke on 01/11/2016.
+//  Copyright © 2016 HicknHack Software GmbH. All rights reserved.
+//
+
+#import "NSData+KPKResize.h"
+
+@implementation NSData (KPKResize)
+
+@end
