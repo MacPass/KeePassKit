96a0bc64d9a21a5c615c3a3204e08aff34b343b7
michael starke <michael.starke@hicknhack-software.com>
2016-11-08 20:12:28 +0100

    test for hmac hashing

--- KeePassKitTests/KPKTestHashedData.m
+++ KeePassKitTests/KPKTestHashedData.m
@@ -22,4 +22,13 @@
   NSData *unhashedData = hashedData.unhashedSha256Data;
   XCTAssertTrue([unhashedData isEqualToData:data], @"Data needs to be the same after hashing and unhashing");
 }
+
+- (void)testHmacSha256Hasing {
+  NSData *data = [NSData dataWithRandomBytes:10000];
+  NSData *key = [NSData dataWithRandomBytes:64];
+  NSError *error;
+  NSData *hashedData = [data hashedHmacSha256DataWithKey:key error:&error];
+  NSData *unhashedData = [hashedData unhashedHmacSha256DataWithKey:key error:&error];
+  XCTAssertEqualObjects(data, unhashedData, @"Hashed and unhashed data are the same");
+}
 @end
