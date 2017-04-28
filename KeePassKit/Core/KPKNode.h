e72bf3869dc3b029ece4840c2e18413aca0ce9cb
michael starke <michael.starke@hicknhack-software.com>
2017-04-28 22:46:10 +0200

    reverting class properties since Xcode 7 does not support them

--- KeePassKit/Core/KPKNode.h
+++ KeePassKit/Core/KPKNode.h
@@ -79,7 +79,7 @@ typedef NS_OPTIONS(NSUInteger, KPKCopyOptions) {
  *	Returns the default icon number for a Group
  *	@return	default icon index for a group
  */
-@property (class, readonly) NSUInteger defaultIcon;
++ (NSUInteger)defaultIcon;
 
 /**
  *  Returns a copy of the node assigned with a new UUID. Use copy on KPKEntry or KPKGroup to get a carbon copy
