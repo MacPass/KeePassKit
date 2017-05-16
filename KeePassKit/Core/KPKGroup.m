c0c44dfaf8614bf17f2997e581bd79494aac47f8
michael starke <michael.starke@hicknhack-software.com>
2017-05-16 11:32:09 +0200

    removed dynamic property declaration

--- KeePassKit/Core/KPKGroup.m
+++ KeePassKit/Core/KPKGroup.m
@@ -48,8 +48,8 @@
 static NSSet *_observedKeyPathsSet;
 
 @dynamic updateTiming;
-@dynamic entries;
-@dynamic groups;
+//@dynamic entries;
+//@dynamic groups;
 
 @synthesize defaultAutoTypeSequence = _defaultAutoTypeSequence;
 @synthesize title = _title;
