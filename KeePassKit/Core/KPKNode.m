ed8d4e66d95bed0acb64f357f472e11fb0f31214
michael starke <michael.starke@hicknhack-software.com>
2017-05-17 11:59:13 +0200

    Fixed error for mutation a enumarted collection

--- KeePassKit/Core/KPKNode.m
+++ KeePassKit/Core/KPKNode.m
@@ -344,19 +344,23 @@
   if(!value) {
     return;
   }
+  //[[self.undoManager prepareWithInvocationTarget:self] addCustomData:value forKey:key];
   [self removeCustomDataObject:[KPKPair pairWithKey:key value:value]];
 }
 
 - (void)addCustomData:(NSString *)value forKey:(NSString *)key {
   if(key && value) {
+    //[[self.undoManager prepareWithInvocationTarget:self] removeCustomDataForKey:key];
     [self addCustomDataObject:[KPKPair pairWithKey:key value:value]];
   }
 }
 
 - (void)clearCustomData {
+  NSMutableSet *pairs = [[NSMutableSet alloc] initWithCapacity:self.mutableCustomData.count];
   for(NSString *key in self.mutableCustomData) {
-    [self removeCustomDataObject:[KPKPair pairWithKey:key value:self.mutableCustomData[key]]];
+    [pairs addObject:[KPKPair pairWithKey:key value:self.mutableCustomData[key]]];
   }
+  [self removeCustomData:pairs];
 }
 
 #pragma mark -
