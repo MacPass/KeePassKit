591eb5dcb106b38446248755c3330dfb0fc95620
michael starke <michael.starke@hicknhack-software.com>
2017-10-25 09:02:32 +0200

    testing HMAC now actually tests something

--- KeePassKitTests/KPKTestKeyComputation.m
+++ KeePassKitTests/KPKTestKeyComputation.m
@@ -18,6 +18,16 @@
 - (void)testHeaderHmac {
   uint8_t dataBytes[64] = {0x00};
   uint8_t keyBytes[32] = {0x00};
+  uint8_t hmacBytes[32] = {
+    0xe9, 0xec, 0xbb, 0xe4,
+    0xbe, 0x33, 0x2d, 0x73,
+    0x87, 0x4b, 0xc4, 0xd1,
+    0x31, 0xcc, 0x86, 0x2b,
+    0x5c, 0xc0, 0xe7, 0x07,
+    0x18, 0x45, 0x48, 0x99,
+    0x14, 0xaa, 0x41, 0x9d,
+    0xfd, 0x77, 0xed, 0xdb
+  };
   
   for(NSUInteger index = 0; index < sizeof(dataBytes); index++) {
     dataBytes[index] = 0x01;
@@ -31,7 +41,8 @@
   NSData *data = [NSData dataWithBytesNoCopy:dataBytes length:sizeof(dataBytes) freeWhenDone:NO];
   
   NSData *hmac = [data kpk_headerHmacWithKey:key];
-  
+  XCTAssertEqual(hmac.length, 32);
+  XCTAssertEqual(0, memcmp(hmac.bytes, hmacBytes, 32));
 }
 
 @end
