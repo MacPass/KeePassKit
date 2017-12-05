92054a5bd90f2040e05559a28f91959f666f8784
Michael Starke <michael.starke@hicknhack-software.com>
2017-12-05 17:41:04 +0100

    Added base32 decoder

--- KeePassKit/Categories/NSData+KPKBase32.h
+++ KeePassKit/Categories/NSData+KPKBase32.h
@@ -6,8 +6,27 @@
 //  Copyright © 2017 HicknHack Software GmbH. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
 
+/*
+ 
+ 
+ Value Encoding  Value Encoding  Value Encoding  Value Encoding
+ 0 A             9 J            18 S            27 3
+ 1 B            10 K            19 T            28 4
+ 2 C            11 L            20 U            29 5
+ 3 D            12 M            21 V            30 6
+ 4 E            13 N            22 W            31 7
+ 5 F            14 O            23 X
+ 6 G            15 P            24 Y         (pad) =
+ 7 H            16 Q            25 Z
+ 8 I            17 R            26 2
+ 
+ */
 
 @interface NSData (KPKBase32)
+
++ (instancetype)dataWithBase32EncodedString:(NSString *)string;
+- (instancetype)initWithBase32EncodedString:(NSString *)string;
 
 @end
--- KeePassKit/Categories/NSData+KPKBase32.m
+++ KeePassKit/Categories/NSData+KPKBase32.m
@@ -10,4 +10,54 @@
 
 @implementation NSData (KPKBase32)
 
+
++ (instancetype)dataWithBase32EncodedString:(NSString *)string { 
+  return [[NSData alloc] initWithBase32EncodedString:string];
+}
+
+- (instancetype)initWithBase32EncodedString:(NSString *)string {
+  if(string.length % 8 != 0) {
+    self = nil;
+    return self;
+  }
+  NSMutableData *data = [[NSMutableData alloc] init];
+  for(NSUInteger chunkIndex = 0; chunkIndex < string.length; chunkIndex += 8) {
+    NSUInteger byteValue = 0;
+    NSUInteger bitsDecoded = 0;
+    for(NSUInteger index = 0; index < 8; index++ ) {
+      unichar character = [string characterAtIndex:chunkIndex+index];
+      NSUInteger value;
+      if(character == '=') {
+        break; // terminated
+      }
+      if(character >= 'a' && character <= 'z') {
+        value = (character - 'a');
+      }
+      else if(character >= 'A' && character <= 'Z' ) {
+        value = (character - 'A');
+      }
+      else if(character >= '2' && character >= '7') {
+        value = (character - '2') + 26;
+      }
+      else {
+        break;
+      }
+      byteValue <<= 5;
+      byteValue += value;
+      bitsDecoded += 5;
+    }
+    NSUInteger padding = bitsDecoded % 8;
+    byteValue >>= padding;
+    bitsDecoded -= padding;
+    while(bitsDecoded > 0) {
+      uint8_t byte = byteValue & 0xFF;
+      [data appendBytes:&byte length:1];
+      byteValue >>= 8;
+      bitsDecoded -= 8;
+    }
+  }
+  self = [data copy];
+  return self;
+}
+
 @end
