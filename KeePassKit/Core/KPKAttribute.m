089f8154721d4f43108e9a85e37540c35bebc77e
Michael Starke <michael.starke@hicknhack-software.com>
2020-07-14 15:51:12 +0200

    Early returns ins setters. Fixed pointer based comparison to use actual equality.

--- KeePassKit/Core/KPKAttribute.m
+++ KeePassKit/Core/KPKAttribute.m
@@ -157,43 +157,47 @@
 }
 
 - (void)setValue:(NSString *)value {
-  if(self.value != value) {
-    if(!self.isDefault) {
-      [(KPKAttribute *)[self.entry.undoManager prepareWithInvocationTarget:self] setValue:self.value];
-      NSString *template = NSLocalizedStringFromTableInBundle(@"SET_CUSTOM_ATTTRIBUTE_%@", nil, [NSBundle bundleForClass:[self class]], @"Action name for setting value of a custom attribute. Contains %@ placeholder");
-      [self.entry.undoManager setActionName:[NSString stringWithFormat:template, self.key ]];
-    }
-    [self.entry touchModified];
-    [self _encodeValue:value];
+  if([self.value isEqualToString:value]) {
+    return;
   }
+  if(!self.isDefault) {
+    [(KPKAttribute *)[self.entry.undoManager prepareWithInvocationTarget:self] setValue:self.value];
+    NSString *template = NSLocalizedStringFromTableInBundle(@"SET_CUSTOM_ATTTRIBUTE_%@", nil, [NSBundle bundleForClass:[self class]], @"Action name for setting value of a custom attribute. Contains %@ placeholder");
+    [self.entry.undoManager setActionName:[NSString stringWithFormat:template, self.key ]];
+  }
+  [self.entry touchModified];
+  [self _encodeValue:value];
 }
 
 - (void)setKey:(NSString *)key {
-  if(![_key isEqualToString:key]) {
-    if([self.entry hasAttributeWithKey:key]) {
-      key = [self.entry proposedKeyForAttributeKey:key];
-    }
-    [[self.entry.undoManager prepareWithInvocationTarget:self] setKey:self.key];
-    [self.entry touchModified];
-    _key = [key copy];
+  if([_key isEqualToString:key]) {
+    return;
   }
+  if([self.entry hasAttributeWithKey:key]) {
+    key = [self.entry proposedKeyForAttributeKey:key];
+  }
+  [[self.entry.undoManager prepareWithInvocationTarget:self] setKey:self.key];
+  [self.entry touchModified];
+  _key = [key copy];
 }
 
 - (void)setProtect:(BOOL)protected {
-  if(_protect != protected) {
-    if(!self.isDefault) {
-      [[self.entry.undoManager prepareWithInvocationTarget:self] setProtect:_protect];
-      if(protected) {
-        NSString *template = [NSString stringWithFormat:NSLocalizedStringFromTableInBundle(@"PROTECT_%@", nil, [NSBundle bundleForClass:self.class], @"Action name for setting a custom string value protected"), self.key];
-        [self.entry.undoManager setActionName:template];
-      }
-      else {
-        NSString *template = [NSString stringWithFormat:NSLocalizedStringFromTableInBundle(@"PROTECT_%@", nil, [NSBundle bundleForClass:self.class], @"Action name for setting a custom string value protected"), self.key];
-        [self.entry.undoManager setActionName:template];
-      }
-    }
-    _protect = protected;
+  if(_protect == protected) {
+    return;
   }
+  if(!self.isDefault) {
+    [[self.entry.undoManager prepareWithInvocationTarget:self] setProtect:_protect];
+    if(protected) {
+      NSString *template = [NSString stringWithFormat:NSLocalizedStringFromTableInBundle(@"PROTECT_%@", nil, [NSBundle bundleForClass:self.class], @"Action name for setting a custom string value protected"), self.key];
+      [self.entry.undoManager setActionName:template];
+    }
+    else {
+      NSString *template = [NSString stringWithFormat:NSLocalizedStringFromTableInBundle(@"PROTECT_%@", nil, [NSBundle bundleForClass:self.class], @"Action name for setting a custom string value protected"), self.key];
+      [self.entry.undoManager setActionName:template];
+    }
+  }
+  _protect = protected;
+  
 }
 
 - (BOOL)isDefault {
