aa0aaafa754b07af86b845f5b2215a99020173aa
Michael Starke <michael.starke@hicknhack-software.com>
2020-11-28 23:09:10 +0100

    documented default values for OTP generator

--- KeePassKit/Cryptography/KPKOTPGenerator.h
+++ KeePassKit/Cryptography/KPKOTPGenerator.h
@@ -34,14 +34,14 @@ typedef NS_ENUM(NSUInteger, KPKOTPGeneratorType) {
 @property (readonly, copy) NSString *string; // will be formatted according to the supplied options on init
 @property (readonly, copy) NSData *data; // will return the raw data of the OTP generator, you normally should only need the string value
 
-@property (copy) NSData *key; // the seed key for the OTP Generator
-@property KPKOTPGeneratorType type; // the type of OTP Generator to use
-@property KPKOTPHashAlgorithm hashAlgorithm; // the hash algorithm to base the OTP data on
-@property NSTimeInterval timeBase; // the base time for Timed OTP
-@property NSUInteger timeSlice; // the time slice for Timed OTP
-@property NSTimeInterval time; // the time to calculate the Timed OTP for
-@property NSUInteger counter; // the counter to calculate the Hmac OTP for
-@property NSUInteger numberOfDigits; // the number of digits to vent as code
+@property (copy) NSData *key; // the seed key for the OTP Generator, default=empty data
+@property KPKOTPGeneratorType type; // the type of OTP Generator to use, default=KPKOTPGeneratorHmacOTP
+@property KPKOTPHashAlgorithm hashAlgorithm; // the hash algorithm to base the OTP data on, default=KPKTOPHashAlgorithmSha1
+@property NSTimeInterval timeBase; // the base time for Timed OTP, default=0 -> unix reference time
+@property NSUInteger timeSlice; // the time slice for Timed OTP, default=30
+@property NSTimeInterval time; // the time to calculate the Timed OTP for, default=0
+@property NSUInteger counter; // the counter to calculate the Hmac OTP for, default=0
+@property NSUInteger numberOfDigits; // the number of digits to vent as code, default=6
 
 @end
 
